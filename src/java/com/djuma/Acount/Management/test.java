a20d6f5fd237be5829395d7666f7b0510bdd76cd
Djuma Suleiman <suldjy5050@yahoo.fr>
2015-03-12 23:38:57 +0200

    made changes

--- src/java/com/djuma/Acount/Management/test.java
+++ src/java/com/djuma/Acount/Management/test.java
@@ -5,6 +5,7 @@
  */
 package com.djuma.Acount.Management;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
@@ -13,27 +14,31 @@ import java.util.Scanner;
  */
 public class test {
     public static void main(String...args){
-        try{
-    Scanner p=new Scanner(System.in);
-String username="";
-String password="";
-String role="";
-    System.out.println("Enter username");
-    username=p.nextLine();
-    System.out.println("Enter the password");
-    password=p.nextLine();
-    System.out.println("Enter the role");
-   role= p.nextLine();
-    User u=new User();
-    u.setUsername(username);
-    u.setPassword(password);
-    u.setConfirmPassword(password);
-    u.setRole(role);
-    u.setDoneBy("djuma");
-    u.createUser();
-    System.out.println("Success");
-        }catch(Exception e){
-        System.out.println(e.getMessage());
-        }
+//        try{
+//    Scanner p=new Scanner(System.in);
+//String username="";
+//String password="";
+//String role="";
+//    System.out.println("Enter username");
+//    username=p.nextLine();
+//    System.out.println("Enter the password");
+//    password=p.nextLine();
+//    System.out.println("Enter the role");
+//   role= p.nextLine();
+//    User u=new User();
+//    u.setUsername(username);
+//    u.setPassword(password);
+//    u.setConfirmPassword(password);
+//    u.setRole(role);
+//    u.setDoneBy("djuma");
+//    u.createUser();
+//    System.out.println("Success");
+//        }catch(Exception e){
+//        System.out.println(e.getMessage());
+//        }
+        
+        double a=100.9456;
+        DecimalFormat j=new DecimalFormat("##");
+        System.out.println(j.format(a));
     }
 }
