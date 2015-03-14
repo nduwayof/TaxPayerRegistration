eec25eaa6f2d76950a48f9e39f60fb9c5e664981
Djuma Suleiman <suldjy5050@yahoo.fr>
2015-03-14 12:35:57 +0200

    Added different models

--- src/java/com/djuma/Individual/Individual_Activity.java
+++ src/java/com/djuma/Individual/Individual_Activity.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.djuma.Enterprise;
+package com.djuma.Individual;
 
 /**
  *
  * @author SULAIMAN
  */
-public class Individual {
+public class Individual_Activity {
     
 }
--- src/java/com/djuma/Tax/TaxPayerType.java
+++ src/java/com/djuma/Tax/TaxPayerType.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.djuma;
+package com.djuma.Tax;
 
 import com.djuma.Connection.SetCon;
 import java.sql.PreparedStatement;
